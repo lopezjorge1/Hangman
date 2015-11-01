8341a1f318112bf6471644d204425be54d6196ae
Jorge <lopez.jorge1@outlook.com>
2015-11-01 12:10:12 -0500

    Add more object oriented code

--- hangman1.rb
+++ hangman1.rb
@@ -1,13 +1,11 @@
-#when I press q and then g it doesn't work correctly
 require './dictionary.rb'
 require './hangman_player.rb'
 class Hangman
-	attr_accessor :secret_word, :body_count, :player
+	attr_accessor :secret_word, :player
 
 	def initialize
 		dictionary = Dictionary.new
 		@secret_word = dictionary.file.readlines.sample
-		@body_count = 8
 		@player = Player.new(secret_word)
 		play
 	end
@@ -18,16 +16,17 @@ class Hangman
 	end
 
 	def is_dead
-		if body_count <= 0
-			puts "I'm sorry, but your game is over. The word was #{secret_word}."
+		if player.body_count <= 0
+			puts "I'm sorry, but your game is over. The word was #{secret_word}"
+			exit
 		else
-			puts "You have #{body_count} tries left."
+			puts "You have #{player.body_count} tries left"
 			continue_game
 		end
 	end
 
 	def continue_game
-		puts "Do you wish to guess the secret word (s) or a letter? (l)\nIf you ever wish to discontinue type quit (q)."
+		puts "Do you wish to guess the secret word (s) or a letter? (l)\nIf you ever wish to discontinue type quit (q)"
 		response = gets.chomp.downcase
 		case response
 		when "secret word","s"
@@ -38,39 +37,18 @@ class Hangman
 			winner
 			is_dead
 		when "quit","q"
-			puts "It was nice having you."
+			puts "It was nice having you"
 		else
-			puts "There's three simple options to choose from. Choose one."
+			puts "There's three simple options to choose from. Choose one"
 			continue_game
 		end
 	end
 
-	def quit
-		puts "Are you trying to quit (q) or is this your guess (g)?"
-		response = gets.chomp.downcase
-		case response
-		when "quit","q"
-			puts "Thank you for playing. Come back soon."
-		when "guess","g"
-			if secret_word.count("q") > 0
-				puts "GREAT! This is what you have thus far: #{guessed_word}."
-				winner
-			else
-				puts "WRONG! TRY HARDER BRUH!"
-				self.body_count -= 1
-				is_dead
-			end
-		else
-			quit
-		end
-	end
-
-
 	def winner
-		if guessed_word == secret_word.downcase
+		if player.guessed_word == secret_word.downcase
 			puts "DING DING DING, YOU DAH WINNAH."
-		else
-			puts "Correct! This is what you have thus far: #{guessed_word}"
+		elsif secret_word.include?(player.letters_guessed.last)
+			puts "Correct! This is what you have thus far: #{player.guessed_word}"
 			continue_game
 		end
 	end
--- hangman_player.rb
+++ hangman_player.rb
@@ -1,12 +1,13 @@
 require './checker.rb'
 class Player
 	include Checker
-	attr_accessor :letters_guessed, :guessed_word, :secret_word
+	attr_accessor :letters_guessed, :guessed_word, :secret_word, :body_count
 
 	def initialize(secret_word) 
 		@letters_guessed = []
 		@guessed_word = secret_word.gsub(/[a-z]/,"*")
 		@secret_word = secret_word
+		@body_count = 5
 	end
 	
 	def guess_word
@@ -22,7 +23,7 @@ class Player
 	end
 
 	def guess_letter
-		puts "What do you think one of the letters are yung buck?\nThese are the letters you've guessed so far: #{letters_guessed}."
+		puts "What do you think one of the letters are yung buck?\nThese are the letters you've guessed so far: #{letters_guessed}"
 		response = gets.chomp.downcase
 		indices = secret_word.chars.each_index.select {|x| secret_word[x] == response}
 		letters_guessed.push(response)
@@ -30,10 +31,29 @@ class Player
 		if response == "q"
 			quit
 		elsif secret_word.include?(response) == false 
-			puts "WRONG! TRY HARDER BRUH."
 			self.body_count -= 1
+			if body_count != 0 then puts "WRONG! TRY HARDER BRUH." end
 		else 
 			indices.each {|x| self.guessed_word[x] = secret_word[x]} 
+		end
+	end
+
+	def quit
+		puts "Are you trying to quit (q) or is this your guess (g)?"
+		response = gets.chomp.downcase
+		case response
+		when "quit","q"
+			puts "Thank you for playing. Come back soon."
+			exit
+		when "guess","g"
+			if secret_word.count("q") > 0
+				puts "GREAT! This is what you have thus far: #{guessed_word}"
+			else
+				puts "WRONG! TRY HARDER BRUH!"
+				self.body_count -= 1
+			end
+		else
+			quit
 		end
 	end
 end
